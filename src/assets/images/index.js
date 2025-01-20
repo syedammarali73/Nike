976da768c93ebcf07452753e276db48aa19576b0
syedammarali73 <syedammarali73@gmail.com>
2025-01-20 23:14:29 +0530

    completed landing page

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -3,12 +3,10 @@ import bigShoe2 from './big-shoe2.png'
 import bigShoe3 from './big-shoe3.png'
 
 import customer1 from './customer1.jpeg'
-import customer2 from './customer2.svg'
+import customer2 from './customer2.png'
 
 import footerLogo from './footer-logo.svg'
 import headerLogo from './header-logo.svg'
-
-import offer from './offer.svg'
 
 import shoe4 from './shoe4.svg'
 import shoe5 from './shoe5.svg'
@@ -20,8 +18,10 @@ import thumbnailBackground from './thumbnail-background.svg'
 import thumbnailShoe1 from './thumbnail-shoe1.svg'
 import thumbnailShoe2 from './thumbnail-shoe2.svg'
 import thumbnailShoe3 from './thumbnail-shoe3.svg'
+import thumbnailShoe4 from './thumbnail-shoe4.png'
 
 export {
+    thumbnailShoe4,
     bigShoe1,
     bigShoe2,
     bigShoe3,
@@ -32,7 +32,6 @@ export {
     footerLogo,
     headerLogo,
 
-    offer,
     shoe4,
     shoe5,
     shoe6,
--- src/sections/CustomerReviews.jsx
+++ src/sections/CustomerReviews.jsx
@@ -1,7 +1,28 @@
+import ReviewCard from "../components/ReviewCard"
+import { reviews } from "../constants"
 
 const CustomerReviews = () => {
   return (
-    <div>CustomerReviews</div>
+    <div className="max-container">
+      <h3 className="font-palanquin text-center text-4xl font-bold">
+        What Our
+        <span className="text-coral-red"> Customers </span>
+        Say?
+      </h3>
+      <p className="info-text m-auto mt-4 max-w-lg text-center">Hear genuine stories from our satisfied customers about their exceptional experiences with us.</p>
+
+      <div className="mt-24 flex flex-1 items-center justify-evenly max-lg:flex-col gap-4">
+        {reviews.map((review, index) => (
+          <ReviewCard 
+          key={index}
+          imgURL={review.imgURL}
+          className={review.customerName}
+          rating={review.rating}
+          feedback={review.feedback}
+          />
+        ))}
+      </div>
+    </div>
   )
 }
 
